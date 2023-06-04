5f3bf82de19ca363e60dcf51ea26a8a656c9c50a
Can's MacBook <macbook@MacBooks-MacBook-Pro-3.local>
2023-06-04 14:08:34 +0700

    upload image with id directory

--- src/main/java/com/webshopproject/admin/user/UserController.java
+++ src/main/java/com/webshopproject/admin/user/UserController.java
@@ -1,5 +1,6 @@
 package com.webshopproject.admin.user;
 
+import com.webshopproject.admin.FileUploadUtil;
 import com.webshopproject.entity.Role;
 import com.webshopproject.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,13 @@ import org.springframework.boot.Banner;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
+import java.io.PrintStream;
 import java.util.List;
 
 @Controller
@@ -41,9 +43,17 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes) {
-        System.out.println(user);
-        userService.save(user);
+    public String saveUser(User user, RedirectAttributes redirectAttributes, @RequestParam("image") MultipartFile multipartFile) throws IOException {
+
+
+        if (!multipartFile.isEmpty()) {
+            String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+            user.setPhotos(fileName);
+            User saveduser = userService.save(user);
+            String uploadDirectory = "user-photos/" + saveduser.getId();
+            FileUploadUtil.saveFile(uploadDirectory, fileName, multipartFile);
+        }
+
         redirectAttributes.addFlashAttribute("message", "The user has been saved successfully");
 
         return "redirect:/users";
