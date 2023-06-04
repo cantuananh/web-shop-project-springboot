42f270296f62778af1cc60f63dc64bdbc5948fbd
Can's MacBook <macbook@MacBooks-MacBook-Pro-3.local>
2023-06-04 09:09:30 +0700

    upload ultil saveFile() function to handle upload file choose when create user

--- src/main/java/com/webshopproject/admin/user/UserController.java
+++ src/main/java/com/webshopproject/admin/user/UserController.java
@@ -1,10 +1,12 @@
 package com.webshopproject.admin.user;
 
+import com.webshopproject.admin.FileUploadUtil;
 import com.webshopproject.entity.Role;
 import com.webshopproject.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -39,9 +42,12 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user, RedirectAttributes redirectAttributes, @RequestParam("image")MultipartFile multipartFile) {
+    public String saveUser(User user, RedirectAttributes redirectAttributes, @RequestParam("image")MultipartFile multipartFile) throws IOException {
         System.out.println(user);
         System.out.println(multipartFile.getOriginalFilename());
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        String uploadDir = "user-photos";
+        FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
 //        userService.save(user);
 //        redirectAttributes.addFlashAttribute("message", "Create user successfully");
 
