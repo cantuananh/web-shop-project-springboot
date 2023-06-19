f68c6bdb58e2552fac6432fc91e8ff72a7a5bcf5
cantuananh <tuananhthemen391997@gmail.com>
2023-06-19 15:10:22 +0700

    create category form

--- src/main/java/com/webshopproject/admin/category/CategoryService.java
+++ src/main/java/com/webshopproject/admin/category/CategoryService.java
@@ -5,7 +5,9 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -15,5 +17,40 @@ public class CategoryService {
 
     public List<Category> getListCategory() {
         return (List<Category>) categoryRepository.findAll();
+    }
+
+    public List<Category> getListCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+        Iterable<Category> categoriesInDB = categoryRepository.findAll();
+
+        for (Category category : categoriesInDB) {
+            if (category.getParent() == null) {
+                categoriesUsedInForm.add(new Category(category.getName()));
+
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    String name = "--" + subCategory.getName();
+                    categoriesUsedInForm.add(new Category(name));
+                    listChildren(categoriesUsedInForm, subCategory, 1);
+                }
+            }
+        }
+
+        return categoriesUsedInForm;
+    }
+
+    public void listChildren(List<Category> categoriesUsedInForm, Category parent, int subLevel) {
+        int newSubLevel = subLevel + 1;
+        Set<Category> children = parent.getChildren();
+        for (Category subCategory : children) {
+            String name = "";
+            for (int i = 0; i < newSubLevel; i++) {
+                name += "--";
+            }
+            name += subCategory.getName();
+            categoriesUsedInForm.add(new Category(name));
+            listChildren(categoriesUsedInForm, subCategory, newSubLevel);
+        }
     }
 }
--- src/test/java/com/webshopproject/admin/category/CategoryRepositoryTests.java
+++ src/test/java/com/webshopproject/admin/category/CategoryRepositoryTests.java
@@ -82,6 +82,5 @@ public class CategoryRepositoryTests {
 
             printChildren(subCategory, newSubLevel);
         }
-
     }
 }
