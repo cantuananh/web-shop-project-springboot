79901ba88fdc5ea6de3d7542d82a30d5539e6b9e
cantuananh <tuananhthemen391997@gmail.com>
2023-07-07 10:53:54 +0700

    tree in category

--- src/main/java/com/webshopproject/entity/Category.java
+++ src/main/java/com/webshopproject/entity/Category.java
@@ -28,6 +28,25 @@ public class Category {
         return copyCategory;
     }
 
+    public static Category copyFull(Category category) {
+        Category copyCategory = new Category();
+        copyCategory.setId(category.getId());
+        copyCategory.setName(category.getName());
+        copyCategory.setImage(category.getImage());
+        copyCategory.setAlias(category.getAlias());
+        copyCategory.setEnabled(category.isEnabled());
+
+        return copyCategory;
+    }
+
+    public static Category copyFull(Category category, String name) {
+        Category copyCategory = Category.copyFull(category);
+
+        copyCategory.setName(name);
+
+        return copyCategory;
+    }
+
     public static Category copyIdAndName(Integer id, String name) {
         Category copyCategory = new Category();
         copyCategory.setId(id);
